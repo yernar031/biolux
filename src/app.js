87f4dcef9ebad289790e0feeb49d16359445f48f
yernar031 <122750806+yernar031@users.noreply.github.com>
2023-01-16 02:26:36 +0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
+app.use('/parse', indexRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
