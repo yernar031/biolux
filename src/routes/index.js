9b8853fbc196ba41fb950ec51f3bcdcdd657e191
yernar031 <122750806+yernar031@users.noreply.github.com>
2023-01-16 02:26:57 +0600

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send({ message: 'Hello world' });
+router.post('/parse', (req, res) => {
+  res.send(req.body);
 });
 
 module.exports = router;
